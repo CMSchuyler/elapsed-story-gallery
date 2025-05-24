a2c46c1028b09d4a6b5e4f0e8e00e6812871d459
Shujun Tian <2777606511@qq.com>
2025-05-24 21:16:40 +0800

    更新相框布局和水面范围

--- src/gallery/Ocean.jsx
+++ src/gallery/Ocean.jsx
@@ -6,44 +6,44 @@ import { Water } from 'three-stdlib';
 extend({ Water });
 
 function Ocean() {
-	const ref = useRef();
-	const gl = useThree((state) => state.gl);
-	const waterNormals = useLoader(
-		THREE.TextureLoader,
-		'./textures/waternormals.jpeg'
-	);
-	waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+  const ref = useRef();
+  const gl = useThree((state) => state.gl);
+  const waterNormals = useLoader(
+    THREE.TextureLoader,
+    './textures/waternormals.jpeg'
+  );
+  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
 
-	const geom = useMemo(() => new THREE.PlaneGeometry(1000, 200), []);
-	const config = useMemo(
-		() => ({
-			textureWidth: 512,
-			textureHeight: 512,
-			waterNormals,
-			sunDirection: new THREE.Vector3(),
-			sunColor: 0xffffff,
-			waterColor: 0x001e0f,
-			distortionScale: 3.7,
-			fog: false,
-			format: gl.encoding,
-		}),
-		[waterNormals]
-	);
+  const geom = useMemo(() => new THREE.PlaneGeometry(2000, 1000), []); // 增加水面范围
+  const config = useMemo(
+    () => ({
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals,
+      sunDirection: new THREE.Vector3(),
+      sunColor: 0xffffff,
+      waterColor: 0x001e0f,
+      distortionScale: 3.7,
+      fog: false,
+      format: gl.encoding,
+    }),
+    [waterNormals]
+  );
 
-	useFrame((state, delta) => {
-		ref.current.material.uniforms.time.value += delta * 0.5;
-	});
+  useFrame((state, delta) => {
+    ref.current.material.uniforms.time.value += delta * 0.5;
+  });
 
-	return (
-		<group position={[0, -2, 0]} rotation={[0.08, 0, 0]}>
-			<water
-				ref={ref}
-				args={[geom, config]}
-				rotation-x={-Math.PI / 2}
-				position-y={0}
-			/>
-		</group>
-	);
+  return (
+    <group position={[0, -2, 0]} rotation={[0.08, 0, 0]}>
+      <water
+        ref={ref}
+        args={[geom, config]}
+        rotation-x={-Math.PI / 2}
+        position-y={0}
+      />
+    </group>
+  );
 }
 
 export default Ocean;
