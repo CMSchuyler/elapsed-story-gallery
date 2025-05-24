dfeb7a47ffaec62b5dac917c6db85015e188dc01
Shujun Tian <2777606511@qq.com>
2025-05-24 21:19:41 +0800

    更新相框布局和水面范围

--- src/gallery/Gallery.jsx
+++ src/gallery/Gallery.jsx
@@ -4,71 +4,81 @@ import { useFrame } from '@react-three/fiber';
 import { useProgress } from '@react-three/drei';
 
 const images = [
-  // Back
-  {
-    position: [-12, 10, 5],
-    rotation: [0, 0, 0],
-    url: './photos/mmexport1689910736872.jpg',
-    title: 'Suzhou river',
-  },
-  {
-    position: [12, 10, 5],
-    rotation: [0, 0, 0],
-    url: './photos/mmexport1689910070987.jpg',
-    title: 'The tree in winter',
-  },
-  // Left Side 1 (近)
-  ...[...Array(10)].map((_, i) => ({
-    position: [-38 - i * 4, 8, 10 + i * 8],
-    rotation: [0, Math.PI / 2.8, 0],
-    url: './photos/3M5A9169.png',
-    title: `Left Near ${i + 1}`,
-  })),
-  // Left Side 2 (远)
-  ...[...Array(10)].map((_, i) => ({
-    position: [-35 - i * 4, 8, 40 + i * 8],
-    rotation: [0, Math.PI / 2.9, 0],
-    url: './photos/3M5A8385.png',
-    title: `Left Far ${i + 1}`,
-  })),
-  // Right Side 1 (近)
-  ...[...Array(10)].map((_, i) => ({
-    position: [38 + i * 4, 8, 10 + i * 8],
-    rotation: [0, -Math.PI / 2.8, 0],
-    url: './photos/mmexport1689910425322.jpg',
-    title: `Right Near ${i + 1}`,
-  })),
-  // Right Side 2 (远)
-  ...[...Array(12)].map((_, i) => ({
-    position: [35 + i * 4, 8, 40 + i * 8],
-    rotation: [0, -Math.PI / 2.9, 0],
-    url: './photos/mmexport1689910497995.jpg',
-    title: `Right Far ${i + 1}`,
-  })),
+	// Back
+	{
+		position: [-12, 10, 5],
+		rotation: [0, 0, 0],
+		url: './photos/mmexport1689910736872.jpg',
+		title: 'Suzhou river',
+	},
+	{
+		position: [12, 10, 5],
+		rotation: [0, 0, 0],
+		url: './photos/mmexport1689910070987.jpg',
+		title: 'The tree in winter',
+	},
+	// Left
+	{
+		position: [-38, 8, 10],
+		rotation: [0, Math.PI / 2.8, 0],
+		url: './photos/3M5A9169.png',
+		title: 'Baoshan temple',
+	},
+	{
+		position: [-35, 8, 40],
+		rotation: [0, Math.PI / 2.9, 0],
+		url: './photos/3M5A8385.png',
+		title: 'Scenes from thirty years ago',
+	},
+	{
+		position: [-53, 8, 46],
+		rotation: [0, Math.PI / 3.5, 0],
+		url: './photos/3M5A7916-1k.png',
+		title: 'Gongqing forest park',
+	},
+	// Right
+	{
+		position: [38, 8, 10],
+		rotation: [0, -Math.PI / 2.8, 0],
+		url: './photos/mmexport1689910425322.jpg',
+		title: 'Huangshan mountains',
+	},
+	{
+		position: [35, 8, 40],
+		rotation: [0, -Math.PI / 2.9, 0],
+		url: './photos/mmexport1689910497995.jpg',
+		title: 'The winter in Donghua University',
+	},
+	{
+		position: [53, 8, 46],
+		rotation: [0, -Math.PI / 3.5, 0],
+		url: './photos/mmexport1689910576643.jpg',
+		title: 'The path in front of my home',
+	},
 ];
 
 const Gallery = () => {
-  const { progress } = useProgress();
-  const [times, setTimes] = useState([]);
+	const { progress } = useProgress();
+	const [times, setTimes] = useState([]);
 
-  useFrame((state, delta) => {
-    const elapsedTime = state.clock.getElapsedTime();
-    if (progress == 100) {
-      if (times.length == 0) setTimes([elapsedTime]);
-      let delay = 8 + times[0];
-      if (elapsedTime < delay) {
-        state.camera.position.z = elapsedTime * (100 / delay);
-        state.camera.rotation.x =
-          -Math.PI * 0.5 + (elapsedTime / delay) * Math.PI * 0.5;
-      }
-    }
-  });
+	useFrame((state, delta) => {
+		const elapsedTime = state.clock.getElapsedTime();
+		if (progress == 100) {
+			if (times.length == 0) setTimes([elapsedTime]);
+			let delay = 8 + times[0];
+			if (elapsedTime < delay) {
+				state.camera.position.z = elapsedTime * (100 / delay);
+				state.camera.rotation.x =
+					-Math.PI * 0.5 + (elapsedTime / delay) * Math.PI * 0.5;
+			}
+		}
+	});
 
-  return (
-    <group position={[0, -0.6, 0]} rotation-x={Math.PI * 0.02}>
-      <Frames images={images} />
-    </group>
-  );
+	return (
+		<group position={[0, -0.6, 0]} rotation-x={Math.PI * 0.02}>
+			<Frames images={images} />
+		</group>
+	);
 };
 
 export default Gallery;
